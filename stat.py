f9164a2778c05de946beadc8be5a6f82051c2ace
zhxZhang <39722133+zhxZhang@users.noreply.github.com>
2018-07-25 20:24:32 +0800

    Update stat.py

--- stat.py
+++ stat.py
@@ -2,19 +2,53 @@ import pandas as pd
 import numpy as np 
 import seaborn as sns
 import matplotlib.pyplot as plt
-import os 
-import missingno as mn
+import os
+import types
+# import missingno as mn
 
 # sns.set_style("whitegrid")
+
 
 def chi_square_test(x, y):
     return 0
 
 
+def cate_int_encoding(data, nan_change_to=np.NaN):
+
+    # Return integer encoding & unique value list
+    # Input can be list OR df
+
+    map = {}
+
+    is_list = type(data) is type([]):
+    if is_list:
+        data = DataFrame({"a": data})
+
+    for col in data.columns:
+        is_category = type(data.ix[0, col]) is type('a')
+        if is_category:
+            data[col], unique = pd.factorize(data[col], na_sentinel=-999)
+            data[col] = data[col].map(lambda x: nan_change_to if x == -999 else x)
+            map[col] = unique
+
+    if is_list:
+        return list(data), map
+    else:
+        return data, map
+
+
 class EDA(object):
 
-    def read_file(self, csv_path):
+    def __init__(self):
+        self.raw_data = None
+        self.vars_info = {}
+        self.num_row = None
+        self.num_col = None
 
+        self.na_stat = None
+        self.na_cols = None
+
+    def read_file(self, csv_path):
         print ('file path:', csv_path)
         self.raw_data = pd.read_csv(csv_path)
         self.vars_info = dict.fromkeys(self.raw_data.columns, {})
@@ -22,53 +56,31 @@ class EDA(object):
         self.num_row, self.num_col = self.raw_data.shape
         print ('shape of df: row %d, col %d .' % (self.num_row, self.num_col))
 
+    def na_detect(self):
 
-    def na_detect(self, output_folder, is_save=False):
+        # NA percent & NA cols
+        na_stat = self.raw_data.isnull().sum()/self.num_row
+        na_cols = na_stat[na_stat > 0].index
+        na_stat = na_stat.reindex(na_cols).dropna().sort_values()
+        print('num of na columns: ', len(na_cols))
 
-        # check folder
-        if not os.path.exists(output_folder):
-            os.makedirs(output_folder)
+        # save info
+        self.na_stat = na_stat
+        self.na_cols = na_cols
 
 
-        # count NA and save NA cols to csv files
-        na = self.raw_data.isnull().sum()/self.num_row
-        na = na.sort_values()
+        # gap = [0, 0.01, 0.2, 0.5, 1]
+        # # keys in na: [ cols name ]
+        # na = {'na{0}_{1}'.format(gap[i], gap[i+1]): [] 
+        #       for i in range(len(gap)-1)}
 
-        indice = na[na>0].index
-        num_na_col = len(indice)
-        na = na.reindex(indice).dropna()
 
-        na_dict = {}
-        for ind, val in na.iteritems():
-            na_dict[ind] = val
+    def pre_process_data(self):
+        cate_int_encoding(self.raw_data)
 
-        print ('num of na cols: ', num_na_col)
 
-        gap = [0, 0.01, 0.2, 0.5, 1]
-        # keys in na: [ cols name ]
-        na = {'na{0}_{1}'.format(gap[i], gap[i+1]): [] 
-              for i in range(len(gap)-1)}
 
-        for k, v in na_dict.items():
-            for i in range(len(gap)):
-                if v <= gap[i]:
-                    tmp = 'na{0}_{1}'.format(gap[i-1], gap[i])
-                    na[tmp].append(k)
-                    break
 
-        # num of NA in different level
-        if is_save:
-            for k, col in na.items():
-                print ('num of %s : %d' % (k, len(col)))
-                self.raw_data[col].to_csv(output_folder + '/' + k + '.csv')
-
-    def count_category(self):
-        # save in vars_info dict
-
-        for var in self.vars_info.keys():
-            tmp = self.raw_data.groupby(var).count()
-            # print (tmp)
-            break
 
 
 
@@ -79,8 +91,8 @@ if __name__ == "__main__":
     folder = 'd:/home_credit_kaggle'
     ap_train_path = folder + '/raw_data/application_train.csv'
 
+
     obj = EDA()
     obj.read_file(ap_train_path)
-    # obj.na_detect(folder + "/na_data")
-    obj.count_category()
-
+    obj.na_detect()
+    obj.pre_process_data()
